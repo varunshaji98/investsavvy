9c5c3dacb89da96627ce20d6d46628776ffbfb58
Varun Shaji <varunshaji@hotmail.com>
2024-03-11 18:36:35 +0800

    Added backend files

---
+++ backend/database.js
@@ -0,0 +1,23 @@
+const mysql = require("mysql");
+require("dotenv").config();
+
+parameters = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  multipleStatements: true,
+};
+
+connection = mysql.createConnection(parameters);
+
+connection.connect((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Connected to MySQL");
+  }
+});
+
+module.exports = { connection };
